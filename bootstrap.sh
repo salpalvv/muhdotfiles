4aae20e21acfe070c33955b22e597a1dfd87bcf6
Daniel Collette <dcollette@navicure.com>
2016-04-07 16:25:07 -0400

    scaffolding out all the random dotfiles i need to fill out

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+
+
+# do some symlinking of dotfiles 
+
+# install installers
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+brew update
+brew upgrade
+
+# install all the things
+#TODO: Make these an array and iterate over it
+brew install git
+brew install wget
+brew install curl
+
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install virtualbox
+brew cask install vlc
+
