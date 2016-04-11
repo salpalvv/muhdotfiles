e0db9685272bd4eaa07f55a3816b58e27b58d0cd
oltp <oltp@queen.navicure.com>
2016-04-11 11:05:02 -0400

    working on the bootstrap symlinking

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,26 @@
 
 
 # do some symlinking of dotfiles 
+ln -s $HOME/muhdotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/muhdotfiles/.vim/ $HOME/.vim
 
 # install installers
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/cask
-brew install brew-cask
-brew tap caskroom/versions
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew tap caskroom/cask
+#brew install brew-cask
+#brew tap caskroom/versions
 
-brew update
-brew upgrade
+#brew update
+#brew upgrade
 
 # install all the things
 #TODO: Make these an array and iterate over it
-brew install git
-brew install wget
-brew install curl
+#brew install git
+#brew install wget
+#brew install curl
 
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install virtualbox
-brew cask install vlc
+#brew cask install dropbox
+#brew cask install google-chrome
+#brew cask install virtualbox
+#brew cask install vlc
 
