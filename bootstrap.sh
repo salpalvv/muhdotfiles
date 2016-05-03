89dc06244e5e1e80e8444aaf6d5e490cc992b6f0
salpalvv <gth701m@gmail.com>
2016-05-02 22:46:14 -0400

    working on mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,53 @@
 #!/usr/bin/env bash
 
-# install ansible based on os {{{
+# bootstrap based on os {{{
 
 # if redhat family {{{
 if [ -f /etc/redhat_release ]; then
-	# install ansible	
-	yum update -y python ruby
-	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-	yum install -y python-setuptools python-yaml python-jinja2 python-paramiko python-crypto libselinux-python ansible
+	echo "redhat"
+	 yum install -y git vim wget curl rsync nc 
 	
-	ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-	cat ~/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-	chmod 600 /root/.ssh
-	chmod 700 /root/.ssh/authorized_keys
+	 # golang setup
 	
-	chmod -x ~/ansible/hosts
+	 # libevent + tmux {{{
+	
+	 pushd ~
+	 
+	 yum install -y ncurses-devel
+	 yum install -y gclibc-static
+	 
+	 wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+	 tar xzvf libevent-2.0.22-stable.tar.gz
+	 cd libevent-2.0.22-stable
+	 ./configure && make && make install
+	 
+	 wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
+	 tar xzvf tmux-2.2.tar.gz
+	 cd tmux-2.2
+	 ./configure && make && make install
+	 popd 
+	 
+	 # }}}
 
 # }}}
 # if debian family {{{
 elif [ -f /etc/debian_version ]; then
-	# install ansible
 	echo "debian"
 # }}}
 # if osx {{{
-elif [ $(uname) == 'FreeBSD' ]; then
-	# install ansible
+elif [ $(uname) == 'Darwin' ]; then
 	echo "mac"
+	# install brew
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew tap caskroom/cask
+	brew install brew-cask
+	brew tap caskroom/versions
+	
+	
 fi
 # }}}
 
 # }}}
-
-# run the bootstrap playbook
-pushd ansible
-ansible-playbook bootstrap.yml
-popd
 
 # do some symlinking of dotfiles {{{ 
 
@@ -51,30 +64,12 @@ ln -sf ~/muhdotfiles/.tmux.conf ~/.tmux.conf
 
 # }}}
 
-#need to distinguish between MAC, DEB, and RHEL family
-#OR just run ansible script. This sounds better
-#now i need to just install ansible on each box depending on OS
-
 # RHEL family specific {{{
 
 # yum install -y git vim wget curl rsync nc 
 
 # golang setup
 
-# ansible setup {{{
-
-# yum update -y python ruby
-# yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-# yum install -y python-setuptools python-yaml python-jinja2 python-paramiko python-crypto libselinux-python ansible
-
-# ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-# cat ~/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-# chmod 600 /root/.ssh
-# chmod 700 /root/.ssh/authorized_keys
-
-# chmod -x ~/ansible/hosts
-
-# }}}
 # libevent + tmux {{{
 
 # pushd ~
