e8fef28fd9ff310acc12ada190b88d85769d06a2
salpalvv <gth701m@gmail.com>
2017-02-12 09:02:01 -0500

    adding gluon plugin

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # install/upgrade homebrew and cask to latest version
 
 # brew install applications
-# cask fzf ripgrep tmux vim git go htop irssi jenkins protobuf python rust tree the_silver_searcher homebrew/dupes/tcpdump
+# cask fzf ripgrep tmux vim git go htop irssi jenkins protobuf python rust tree the_silver_searcher homebrew/dupes/tcpdump neovim/neovim/neovim
 
 # brew cask install applications
 # firefox iterm2 nmap steam virtualbox vagrant google-chrome deluge skype discord android-studio lyx avira-antivirus dropbox the-unarchiver vlc vmware-fusion flux malwarebytes-anti-malware
